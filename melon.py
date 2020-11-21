5a2ead89ae7a88ed2f480c5fb5ed590544d5cad3
huouvcti <huouvcti@gmail.com>
2020-11-21 20:30:34 +0900

    melon chart

---
+++ melon.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup as bs
+
+
+driver = webdriver.Chrome(executable_path="chromedriver.exe")
+
+url = "https://www.melon.com/chart/day/index.htm"
+
+driver.get(url)
+html = driver.page_source
+soup = bs(html, "html.parser")
+
+
+rank_list = soup.find_all("span", {"class": "rank"})
+title_list = soup.find_all("div", {"class": "ellipsis rank01"})
+singer_list = soup.find_all("span", {"class": "checkEllipsis"})
+
+data = []
+for i in range(100):
+    data.append([i+1, title_list[i].text.replace("\n",""), singer_list[i].text.replace("\n","")])
+    print(str(data[i][0])+"위", data[i][1], data[i][2])
